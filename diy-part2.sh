05b85de7bb00beff2ac972847e3ab721bc113693
pop098 <65938943+pop098@users.noreply.github.com>
2021-03-31 19:50:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,7 +28,6 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='sunny-115'' pac
 # 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 sed -i "s/OpenWrt /Sunny $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
-
 cd openwrt
 rm -rf package/lean/luci-theme-argon  #删除源码自带的argon主题，因为最下面一个链接是增加了其他作者制作的argon主题
 
